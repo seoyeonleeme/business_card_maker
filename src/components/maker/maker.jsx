30c70a255aaee13af67359d84db1603362df0ba3
Seoyeon Lee <67448999+seoyeonleeme@users.noreply.github.com>
2021-08-20 14:11:25 +0900

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({authService}) => {
-    const [cards, setCards] = useState([
-        {
+    const [cards, setCards] = useState({
+        '1': {
             id: '1',
             name: 'Seoyeon',
             company: 'SpaceX',
@@ -19,7 +19,7 @@ const Maker = ({authService}) => {
             fileName: 'seoyeon',
             fileURL: null,
         },
-        {
+        '2': {
             id: '2',
             name: 'Elon Musk',
             company: 'Solarcity',
@@ -30,7 +30,7 @@ const Maker = ({authService}) => {
             fileName: 'elon',
             fileURL: process.env.PUBLIC_URL + '/images/elonmusk.jpg',
         },
-        {
+        '3': {
             id: '3',
             name: 'Henry Cavill',
             company: 'Tesla Motors',
@@ -40,8 +40,9 @@ const Maker = ({authService}) => {
             message: 'Go for it',
             fileName: 'henry',
             fileURL: process.env.PUBLIC_URL + '/images/henrycavill.jpg',
-        }
-    ]);
+        },
+    });
+
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -55,16 +56,27 @@ const Maker = ({authService}) => {
         });
     });
 
-    const addCard = card => {
-        const updated = [...cards, card];
-        setCards(updated);  
+    const createOrUpdateCard = card => {
+        setCards(cards => {
+            const updated = {...cards};
+            updated[card.id] = card;
+            return updated;
+        });
+    };
+
+    const deleteCard = card => {
+        setCards(cards => {
+            const updated = {...cards};
+            delete updated[card.id];
+            return updated;
+        });
     };
 
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor cards ={cards} addCard={addCard}/>
+                <Editor cards ={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard}/>
                 <Preview cards ={cards}/>
             </div>
             <Footer />

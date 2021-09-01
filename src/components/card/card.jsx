603972077a450e5a8902fd7c4cbe95d814814be5
Seoyeon Lee <67448999+seoyeonleeme@users.noreply.github.com>
2021-09-01 13:50:11 +0900

    use memo and useCallback

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './card.module.css';
 
 const DEFAULT_IMAGE = process.env.PUBLIC_URL + '/images/default_logo.png';
 
-const Card = ({ card }) => {
+const Card = memo(({ card }) => {
   const {
     name,
     company,
@@ -26,7 +26,7 @@ const Card = ({ card }) => {
       </div>
     </li>
   );
-};
+});
 
 function getStyles(theme) {
   switch (theme) {
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './header.module.css';
 
-const Header = ({onLogout}) => (
+const Header = memo(({onLogout}) => (
     <header className={styles.header}>
         {onLogout && <button className={styles.logout} onClick={onLogout}>Logout</button>}
         <img className={styles.logo} src= {process.env.PUBLIC_URL + "/images/logo.png"} alt="logo" />
         <h1 className={styles.title}>Seoyeon's Business Card Maker</h1>
     </header>
-);
+));
 
 export default Header;
 

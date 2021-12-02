fa2febde41eadfd97c3067701f2345e3cd0c1189
Seoyeon Lee <67448999+seoyeonleeme@users.noreply.github.com>
2021-12-02 19:18:27 +0900

    saving to github

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,8 @@ import styles from './app.module.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Maker from './components/maker/maker';
 
+//console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+
 function App({ FileInput, authService, cardRepository }) {
   return (
     <div class={styles.app}>
